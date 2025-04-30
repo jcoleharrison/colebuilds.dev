0d94474f72c0f97b382ad8ecd6752bdf4bff1abf
Cole Harrison <j.cole.harrison@me.com>
2025-04-30 16:24:14 -0500

    update featured

--- src/static-data/featured-cards-data.ts
+++ src/static-data/featured-cards-data.ts
@@ -1,25 +1,24 @@
 export const cardData = [
   {
-    title: 'ditto',
+    title: "ditto",
     description:
-      'The simplest self-building coding agent (~500 lines of code) that builds itself into a multi-file Python Flask app from a simple prompt.',
+      "The simplest self-building coding agent (~500 lines of code) that builds itself into a multi-file Python Flask app from a simple prompt.",
     imageLink:
-      'https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    externalLink: 'https://www.google.com/',
+      "https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    externalLink: "https://www.google.com/",
   },
   {
-    title: 'ditto',
-    description: 'The simplest self-building coding agent',
+    title: "ditto",
+    description: "The simplest self-building coding agent",
     imageLink:
-      'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    externalLink: 'https://www.google.com/',
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    externalLink: "https://www.google.com/",
   },
   {
-    title: 'ditto',
+    title: "Alpha Signal",
     description:
-      'The simplest self-building coding agent (~500 lines of code) that builds itself into a multi-file Python Flask app from a simple prompt.',
-    imageLink:
-      'https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    externalLink: 'https://www.google.com/',
+      "Alpha Signal is a crypto trading platform that enables uses to use social signals and AI to automate trades, giving disproportionate gains and minimal losses.",
+    imageLink: "/blockchain.png", // Assuming blockchain.png is in the public folder
+    externalLink: "https://alphasignalcrypto.com/",
   },
 ];
